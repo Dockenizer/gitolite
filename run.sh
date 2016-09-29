ac5ba49791ec41d50159acad7643587270719279
Jacques Moati <jacques@moati.net>
2016-09-29 18:25:44 +0200

    First version

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+if ! ls /etc/ssh/ssh_host_* 1> /dev/null 2>&1; then
+  ssh-keygen -A
+fi
+
+if [ ! -d /home/git/.gitolite ]; then
+    if [ ! -f /tmp/admin.pub ]; then
+      echo "/tmp/admin.pub is missing !"
+      exit 1
+    fi
+
+    chown -R git:git ~git
+    sudo -H -u git sh -c 'mkdir ~/bin'
+    sudo -H -u git sh -c 'git clone git://github.com/sitaramc/gitolite ~/gitolite'
+    sudo -H -u git sh -c '~/gitolite/install -ln'
+    sudo -H -u git sh -c '~/bin/gitolite setup -pk /tmp/admin.pub'
+else
+  sudo -H -u git sh -c '~/bin/gitolite setup'
+fi
+
+/usr/sbin/sshd -D
